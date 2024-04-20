3d2ad77cbfc78f3a0e6edfce6009a658fec8a747
giyomdabin <155427024+giyomdabin@users.noreply.github.com>
2024-04-20 22:28:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.79 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i < food.length; i++){
+            int count = food[i] / 2;
+            
+            for(int j = 0; j < count; j++){
+                sb.append(i);
+            }
+        }
+        answer = sb.toString() + "0" + sb.reverse().toString();
+        
+        return answer;
+    }
+}
