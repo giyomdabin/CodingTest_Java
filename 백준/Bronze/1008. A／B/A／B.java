8578792c8b7f394757ad9fd09425c2553c1493b3
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2024-11-12 01:04:14 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -3,14 +3,12 @@ import java.io.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         double A = Double.parseDouble(st.nextToken());
         double B = Double.parseDouble(st.nextToken());
-        bw.write(Double.toString(A/B));
-        bw.close();
+        System.out.println(A/B);
         br.close();
     }
 }
