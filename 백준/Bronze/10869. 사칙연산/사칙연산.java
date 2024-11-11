13e49b8c7629862ab5cd86b226cfc260fc332c41
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2024-11-12 01:05:17 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString(A+B) + "\n");
+        bw.write(Integer.toString(A-B) + "\n");
+        bw.write(Integer.toString(A*B) + "\n");
+        bw.write(Integer.toString(A/B) + "\n");
+        bw.write(Integer.toString(A%B) + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
