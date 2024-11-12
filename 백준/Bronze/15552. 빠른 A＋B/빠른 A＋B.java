eb605996ab2442d3708f107a551e3e7c73c7d760
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2024-11-13 01:43:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 880 ms, Memory: 289780 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            bw.write(a+b+"\n");
+        }
+        br.close();
+        bw.close();
+    }
+}
