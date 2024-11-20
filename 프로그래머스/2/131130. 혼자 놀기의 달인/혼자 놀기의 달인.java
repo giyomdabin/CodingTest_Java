ab1dab7878bacab7aa09dff13b76e23a41e0869c
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2024-11-20 18:30:29 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 2.31 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int[] cards) {
-        int result = 0;
         int[] visited = new int[cards.length + 1];
 
-        List<Integer> list = new ArrayList<>();
+        List<Integer> groups = new ArrayList<>();
 
+        int current = 0;
         for (int i = 0; i < cards.length; i++) {
             if (visited[cards[i]] == 1) continue;
-            int current = cards[i];
+            current = cards[i];
             int count = 0;
 
             while (true) {
@@ -19,13 +19,13 @@ class Solution {
                 visited[current] = 1;
                 current = cards[current - 1];
             }
-            list.add(count);
+            groups.add(count);
         }
 
-        if (list.size()  <= 1) return 0;
+        if (groups.size()  <= 1) return 0;
 
-        Collections.sort(list, Collections.reverseOrder()); // 내림차순으로 정렬
-        result = list.get(0) * list.get(1);
+        Collections.sort(groups, Collections.reverseOrder()); // 내림차순으로 정렬
+        int result = groups.get(0) * groups.get(1);
         return result;
     }
 }
