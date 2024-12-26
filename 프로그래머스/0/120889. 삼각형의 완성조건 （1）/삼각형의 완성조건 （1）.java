e6402d8c1c2d5f154fee2ede5a75f2c8b234516e
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2024-12-26 23:08:26 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.48 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 1;
+        
+        Arrays.sort(sides);
+        
+        if(sides[0] + sides[1] > sides[2]) return answer;
+        else{
+            answer = 2;
+            return answer;
+        }
+    }
+}
