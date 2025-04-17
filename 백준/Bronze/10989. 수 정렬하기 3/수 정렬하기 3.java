cd04af7444ae179f88e818e9994dc5d0e9338a80
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2025-04-17 17:10:42 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2456 ms, Memory: 345636 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        for(int i : nums) bw.write(i + "\n");
+        bw.close();
+    }
+}
