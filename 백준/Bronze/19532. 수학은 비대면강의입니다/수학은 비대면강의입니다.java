3b615942d5191fec71e3d1e4c91a130529a3d039
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2025-04-30 11:49:20 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 120 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        bw.write((c * e- b * f) / (a * e - b * d) + " "); //x
+        bw.write((c * d - a * f) / (b * d - a * e) +""); // y
+        
+        bw.close();
+    }
+}
