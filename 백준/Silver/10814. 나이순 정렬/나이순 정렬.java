b1dff1599f9b0d2355f5014490c294ab85593f8b
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2025-05-06 10:12:47 +0900

    [Silver V] Title: 나이순 정렬, Time: 612 ms, Memory: 49920 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = st.nextToken();	// 나이
+            arr[i][1] = st.nextToken();	// 이름
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            // 나이순으로 정렬
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+
+        bw.write(sb + "");
+        bw.close();
+    }
+}
