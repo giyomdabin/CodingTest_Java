151b46a8c43a382f087b35c320d7bdd283760dcc
Dabin Jang <155427024+giyomdabin@users.noreply.github.com>
2025-05-10 21:45:40 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int n = Integer.parseInt(br.readLine());
+ 
+		System.out.println(fibonacci(n));
+ 
+	}
+ 
+	// 피보나치 함수
+	static int fibonacci(int n) {
+		if (n == 0)	return 0;
+		if (n == 1)	return 1;
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
